eca0c9a7c5da5c392c03d5607e39fb60c8dcb4d5
YingMuo <wl03452329@gmail.com>
2021-02-06 16:56:12 +0800

    add main

---
+++ sandbox.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+#include <unistd.h>
+
+#include <signal.h>
+#include <pthread.h>
+#include <sys/wait.h>
+#include <sys/resource.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+int main()
+{
+    return 0;
+}
